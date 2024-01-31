c0d4bb01b7573b6ec597587b20b7ebc448250751
Ayush Kamble <aykkamble098@gmail.com>
2024-01-31 23:20:46 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {environment} from '../../src/config';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'admin';
+  apiurl = environment.apiUrl;
+
+
 }
